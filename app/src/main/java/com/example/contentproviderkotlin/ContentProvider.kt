cf81bc424fd2eb69256cda10692793df0a006942
Bilal Yousaf <F2017266297@umt.edu.pk>
2021-03-17 11:44:53 +0500

    Initial commit

---
+++ app/src/main/java/com/example/contentproviderkotlin/ContentProvider.kt
@@ -0,0 +1,47 @@
+package com.example.contentproviderkotlin
+
+import android.content.ContentProvider
+import android.content.ContentValues
+import android.database.Cursor
+import android.net.Uri
+
+class ContentProvider: ContentProvider() {
+    override fun onCreate(): Boolean {
+        return false
+    }
+
+    override fun query(
+        uri: Uri,
+        projection: Array<String?>?,
+        selection: String?,
+        selectionArgs: Array<String?>?,
+        sortOrder: String?
+    ): Cursor? {
+        return null
+    }
+
+    override fun getType(uri: Uri): String? {
+        return null
+    }
+
+    override fun insert(uri: Uri, values: ContentValues?): Uri? {
+        return null
+    }
+
+    override fun delete(
+        uri: Uri,
+        selection: String?,
+        selectionArgs: Array<String?>?
+    ): Int {
+        return 0
+    }
+
+    override fun update(
+        uri: Uri,
+        values: ContentValues?,
+        selection: String?,
+        selectionArgs: Array<String?>?
+    ): Int {
+        return 0
+    }
+}
